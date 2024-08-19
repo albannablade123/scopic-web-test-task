499f78e7d23d08a368ed50cf9f37d650403adc14
albannablade123 <hasanal082000@gmail.com>
2024-08-19 14:12:19 +0300

    - chore: Init docker for Backend

--- auction_web_api/my_app/utils/process_bid.py
+++ auction_web_api/my_app/utils/process_bid.py
@@ -10,14 +10,12 @@ def process_auto_bids(item_id, triggering_user_id=None):
     # Fetch the current highest bid for the item    
     # Fetch active auto-bids for the item
     auto_bids = Autobid.objects.filter(item_id=item_id, auto_bidding_active=True).order_by('created_at')
-    print("checkpoint-1")
     for auto_bid in auto_bids:
 
         max_bid_amount = auto_bid.max_bid_amount
         current_auto_bid_amount = auto_bid.current_auto_bid_amount
         alert_percentage = Decimal(auto_bid.auto_bid_alert_percentage)  # Convert to Decimal
         user_id = auto_bid.user.id  # Fetch the user ID for notifications
-        print("checkpoint-2")
 
         # Fetch the highest bid amount currently placed for the item
         highest_bid = Bid.objects.filter(item_id=item_id).order_by('-amount').first()
@@ -25,11 +23,9 @@ def process_auto_bids(item_id, triggering_user_id=None):
 
         bid_increment = Decimal('1.00')  # Ensure bid increment is Decimal
         new_bid_amount = highest_bid_amount + bid_increment
-        print("checkpoint-3A", highest_bid.amount, new_bid_amount)
 
         # Ensure the new bid amount does not exceed the max bid amount
         if new_bid_amount <= max_bid_amount and new_bid_amount > current_auto_bid_amount and user_id != triggering_user_id:
-            print(user_id, triggering_user_id, "Checkpoint-3b")
             # Create a new bid for this auto-bid
             new_bid_data = {
                 'item': item_id,
@@ -37,7 +33,6 @@ def process_auto_bids(item_id, triggering_user_id=None):
                 'user': auto_bid.user.id,  # Ensure the bid is created on behalf of the user
                 'auto_bidding': True
             }
-            print("checkpoint-4")
 
             new_bid_serializer = BidSerializer(data=new_bid_data)
             if new_bid_serializer.is_valid():
@@ -48,7 +43,6 @@ def process_auto_bids(item_id, triggering_user_id=None):
                     # Update auto-bid with the new amount
                     auto_bid.current_auto_bid_amount = new_bid_amount
                     auto_bid.save()
-                    print("checkpoint-5")
 
                     # Check if the threshold percentage is reached
                     threshold_amount = (alert_percentage / Decimal('100')) * max_bid_amount
