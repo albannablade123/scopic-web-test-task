2d677b4b8a6ecc9eb8528a2f6d414f8836983682
albannablade123 <hasanal082000@gmail.com>
2024-08-24 15:22:57 +0300

    - chore: changes on entrypoint.sh

--- auction_web_api/entrypoint.sh
+++ auction_web_api/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
-
-if [ "$1" = 'celery' ]; then
-  shift  # Remove 'celery' from the argument list
-  celery --app=auction_web_api worker -l DEBUG "$@"
-else
-  ls
-  python manage.py migrate --no-input
-  python manage.py collectstatic --no-input
-  python manage.py loaddata users.json
-  python manage.py loaddata items.json
-
-  uvicorn auction_web_api.asgi:application --host 0.0.0.0 --port 8000
-fi
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+python manage.py loaddata users.json
+python manage.py loaddata items.json
+uvicorn auction_web_api.asgi:application --host 0.0.0.0 --port 8000
